9403177a5c5dda009b672580a928a992e1f41823
aivavic <afanasjev-v@yandex.ru>
2015-03-12 11:18:50 +0200

    Добавлениеdiv.view

--- protected/views/tvseries/update.php
+++ protected/views/tvseries/update.php
@@ -1,21 +1,10 @@
 <?php
 /* @var $this TvseriesController */
 /* @var $model Tvseries */
-
-$this->breadcrumbs=array(
-	'Tvseries'=>array('index'),
-	$model->name=>array('view','id'=>$model->id),
-	'Update',
-);
-
-$this->menu=array(
-	array('label'=>'List Tvseries', 'url'=>array('index')),
-	array('label'=>'Create Tvseries', 'url'=>array('create')),
-	array('label'=>'View Tvseries', 'url'=>array('view', 'id'=>$model->id)),
-	array('label'=>'Manage Tvseries', 'url'=>array('admin')),
-);
 ?>
 
-<h1>Update Tvseries <?php echo $model->id; ?></h1>
 
+<h1>Update Tvseries <?php echo $model->id; ?></h1>
+<div class="view">
 <?php $this->renderPartial('_form', array('model'=>$model)); ?>
+</div>
