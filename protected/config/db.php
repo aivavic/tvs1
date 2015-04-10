330fc7498ba2904dbab017eb0e0e3977befb1a0b
Viktor Afanasjev <afanasjev-v@yandex.ru>
2015-04-10 15:24:51 +0300

    Delete db.php

--- protected/config/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-    'connectionString' => 'mysql:host=host;dbname=dbname',
-    'emulatePrepare' => true,
-    'username' => 'bratskas_tvs',
-    'password' => 'password',
-    'charset' => 'utf8',
-);
